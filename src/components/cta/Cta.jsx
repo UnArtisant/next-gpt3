dfcafe80d279d384e345b01cfdfe2ca0d4334b3e
UnArtisant <73105273+UnArtisant@users.noreply.github.com>
2021-12-22 15:29:55 +0100

    cta

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import "./Cta.module.css"
+import style from "./Cta.module.css"
 
 function Cta() {
     return (
-        <div>
-            
+        <div className="px-24 my-24">
+            <div className={`p-10 ${style["gtp3__cta-container"]}`}>
+                <p className="text-sm my-2">Request Early Access to Get Started</p>
+                <div className="flex flex-col lg:flex-row lg:items-center justify-between my-4">
+                    <h1 className="text-2xl font-bold">Register today & start exploring the endless possiblities.</h1>
+                    <button className="bg-black rounded-full my-4 lg:my-0 px-6 text-lg py-2 text-white">Get Started</button>
+                </div>
+            </div>
         </div>
     )
 }
--- src/layout/possibility/Possibility.jsx
+++ src/layout/possibility/Possibility.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-import "./Possibility.module.css"
+import style from "./Possibility.module.css"
+import possibility from "../../assets/possibility.png"
 function Possibility() {
     return (
-        <div>
-            
+        <div className="px-24 flex flex-col md:flex-row items-center justify-center">
+            <div className="flex items-center md:mr-20 max-w-2xl justify-start ">
+                <img src={possibility.src} alt="possibility" />
+            </div>
+            <div  className="flex justify-end items-start flex-col max-w-2xl">
+                <h4 style={{color : "#71E5FF"}} className="text-lg my-4" >Request Early Access to Get Started</h4>
+                <h1 className={`${style["gpt3__possibility-heading"]} my-4 text-5xl font-bold`}>The possibilities are beyond your imagination</h1>
+                <p className={`${style["gpt3__possibility-description"]} my-4`}>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+                <a style={{color: "#FF8A71"}} className="my-3" href="#">Request Early Access to Get Started</a>
+            </div>
         </div>
     )
 }
